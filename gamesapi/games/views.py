299ce24ca6c009e70228242c1d480435f2f246bf
sanix sandel <sanicksikani@gmail.com>
2020-07-01 12:07:08 +0300

    working with CBV

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -1,56 +1,58 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from games.models import Game
-from games.serializers import GameSerializer
+from games.models import (
+    GameCategory,
+    Game,
+    Player,
+    PlayerScore,
+)
+from games.serializers import (
+    GameCategorySerializer, 
+    GameSerializer,
+    PlayerSerializer,
+    PlayerScoreSerializer
+)    
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content=JSONRenderer().render(data)
-        kwargs['content_type']='application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class GameCategoryList(generics.ListCreateAPIView):
+    queryset=GameCategory.objects.all()
+    serializer_class=GameCategorySerializer
+    name='gamecategory-list'
 
 
-@csrf_exempt
-def game_list(request):
-    if request.method=='GET':
-        games=Game.objects.all()
-        games_serializer=GameSerializer(games, many=True)
-        return JSONResponse(games_serializer.data)
-
-    elif request.method=='POST':
-        game_data=JSONParser().parse(request)
-        game_serializer=GameSerializer(data=game_data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return JSONResponse(game_serializer.data, status=status.HTTP_201_CREATED)
-        return JSONResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=GameCategory.objects.all()
+    serializer_class=GameCategorySerializer
+    name='gamecategory-detail'
 
 
-@csrf_exempt
-def game_detail(request, pk):
-    try:
-        game=Game.objects.get(pk=pk)
-    except Game.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-    if request.method=='GET':
-        game_serializer=GameSerializer(game)
-        return JSONResponse(game_serializer.data)
-    elif request.method=='PUT':
-        game_data=JSONParser().parse(request)
-        game_serializer=GameSerializer(game, data=game_data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return JSONResponse(game_serializer.data)
-        return JSONResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method=='DELETE':
-        game.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)      
+class GameList(generics.ListCreateAPIView):
+    queryset=Game.objects.all()
+    serializer_class=GameSerializer
+    name='game-list'
 
-#The code uses the @csrf_exempt decorator in the two functions to ensure 
-# that the view sets a Cross-Site Request Forgery (CSRF) cookie.                       
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Game.objects.all()
+    serializer_class=GameSerializer
+    name='game-detail'
 
+class PlayerList(generics.ListCreateAPIView):
+    queryset=Player.objects.all()
+    serializer_class=PlayerSerializer
+    name='player-list'
+
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Player.objects.all()
+    serializer_class=PlayerSerializer
+    name='player-detail'
+
+class PlayerScoreList(generics.ListCreateAPIView):
+    queryset=PlayerScore.objects.all()
+    serialzer_class=PlayerScoreSerializer
+    name='Playerscore-list'
+
+class playerScoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=PlayerScore.objects.all()
+    serializer_class=PlayerScoreSerializer
+    name='playerscore-list'                       
